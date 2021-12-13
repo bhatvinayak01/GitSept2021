88238d139057ff775c1a0f1c5848d628019552a2
bhatvinayak01 <bhatvinayak01@gmail.com>
2021-12-13 11:14:24 +0530

    modified

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 class Demo
 {
-public static void main(String args[])
-{
-System.out.println("Hello Git");
-}
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Git !!");
+	}
 }
